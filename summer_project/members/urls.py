222f4923d2fd72da465b0af2947f1bc652a6f92e
hanasoliman72 <hanasmsalah105@gmail.com>
2025-08-23 17:20:36 +0300

    Hana trying something

--- summer_project/members/urls.py
+++ summer_project/members/urls.py
@@ -32,5 +32,4 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='password_reset_complete.html'
     ), name='password_reset_complete'),
-
 ]
