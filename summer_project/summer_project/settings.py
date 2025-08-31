356cb7b48137d785b8a44b15fda53ffb6eaba1fe
hanasoliman72 <hanasmsalah105@gmail.com>
2025-08-31 19:58:48 +0300

    fix front url 2

--- summer_project/summer_project/settings.py
+++ summer_project/summer_project/settings.py
@@ -61,14 +61,12 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://complaintsystem-git-main-sohila24ahmed-gmailcoms-projects.vercel.app",
     "https://complaintsystem-steel.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://complaintsystem-git-main-sohila24ahmed-gmailcoms-projects.vercel.app",
     "https://complaintsystem-steel.vercel.app",
 ]
 
