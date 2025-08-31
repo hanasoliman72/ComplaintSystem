e23140a35cfb17a93cbd6e106b786fc2010720f4
hanasoliman72 <hanasmsalah105@gmail.com>
2025-08-31 18:03:17 +0300

    edit signals.py

--- summer_project/members/signals.py
+++ summer_project/members/signals.py
@@ -1,10 +1,12 @@
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 from members.models import User
+import os
 
 @receiver(post_migrate)
 def create_default_superuser(sender, **kwargs):
     if not User.objects.filter(username="admin").exists():
+        admin_password = os.getenv("DJANGO_ADMIN_PASSWORD")
         user = User(
             username="admin",
             email="hanasmsalah105@gmail.com",
@@ -13,6 +15,5 @@ def create_default_superuser(sender, **kwargs):
             is_superuser=True,
             is_staff=True,
         )
-        user.set_password("Admin1234")
+        user.set_password(admin_password)
         user.save()
-        print("Default superuser created: admin / Admin1234")
